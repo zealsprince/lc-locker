329495c36960661b0dbf96ba5f94bfed2aac6739
Andrew Lake <andrew@zealsprince.com>
2024-02-25 17:17:36 +0100

    Complete most behaviour for the enemy

--- Locker/MonoBehaviours/LockerAI.cs
+++ Locker/MonoBehaviours/LockerAI.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections.Generic;
+using GameNetcodeStuff;
 using UnityEngine;
 using UnityEngine.VFX;
 
 namespace Locker.MonoBehaviours
 {
-    public class LockerAI : MonoBehaviour
+    public class LockerAI : EnemyAI
     {
         public enum LockerState : ushort
         {
+            Debug = 0,
             Dormant = 1,
-            Active = 2,
+            Activating = 2,
             Chasing = 3,
-            Closing = 4,
+            Resetting = 4,
             Consuming = 5,
         }
 
@@ -47,27 +49,57 @@ namespace Locker.MonoBehaviours
         private static readonly string consumeVFXBeginTriggerName = "BeginConsume";
         private static readonly string consumeVFXEndTriggerName = "EndConsume";
 
-        private float Speed = 1f;
-
-        // Store the last state for easier transitions.
+        // Last state for easier transitions.
         private LockerState lastState;
 
         // Store the current target player and last target chase location.
-        private GameObject targetPlayer;
         private Vector3 targetLocation;
+        private Quaternion targetRotation;
 
-        // Store the current eye color and intensity.
-        private Color currentEyeColor;
-        private float currentEyeIntensity;
+        // Momentary rotational force of the enemy when targeting.
+        private float currentRotationSpeed = 0f;
+        private readonly float maxRotationSpeed = 60f;
+
+        // Momentary speed values of the enemy.
+        private float currentSpeed = 0f;
+        private readonly float maxSpeed = .75f;
+
+        // Current eye color and intensity.
+        private Color currentEyeColor = eyeColorDormant;
+        private float currentEyeIntensity = 0;
+
+        // Track the duration between starting a chase.
+        private float activationTimer = 0f;
+        private readonly float activationDuration = 1.5f;
+
+        // Duration before the locker begins rotating towards the target.
+        private readonly float activationSpinWindup = 0.45f;
+
+        // Track the duration between finishing a chase and consuming a player.
+        private float consumeTimer = 0f;
+        private readonly float consumeDuration = 2.2f;
+
+        // Duration before the locker begins the blood gushing effect to sync up with audio.
+        private readonly float consumeBloodWindup = 1f;
+        private bool consumeBloodTriggered = false;
+
+        // Track the duration between finishing a chase and consuming a player.
+        private float resetTimer = 0f;
+        private readonly float resetDuration = 1f;
 
         // Store the current state publicly so I can switch it in the inspector.
         public LockerState state;
+        public bool DebugToCamera = false;
 
-        void Start()
+        public AudioClip AudioClipPing;
+        public AudioClip AudioClipActivate;
+        public AudioClip AudioClipChase;
+        public AudioClip AudioClipReset;
+        public AudioClip AudioClipConsume;
+
+        public override void Start()
         {
-            // Set the default eye color and intensity.
-            currentEyeColor = eyeColorDormant;
-            currentEyeIntensity = 0;
+            base.Start();
 
             // Assign our components.
             audioSource = GetComponent<AudioSource>();
@@ -110,7 +142,6 @@ namespace Locker.MonoBehaviours
             internalLight.intensity = 0;
             internalLight.enabled = true;
 
-            targetPlayer = gameObject;
             targetLocation = Vector3.zero;
 
             // Capture all visual effects that are a part of this.
@@ -126,18 +157,53 @@ namespace Locker.MonoBehaviours
             SwitchState(LockerState.Dormant);
         }
 
-        private void Update()
+        public override void DoAIInterval()
         {
+            base.DoAIInterval();
+
+            // Get the closest player if possible.
+            PlayerControllerB closestPlayer = GetClosestPlayer(true, true, true);
+            if (closestPlayer != null)
+            {
+                // If their flashlight is out and activate. Chase.
+                if (closestPlayer.pocketedFlashlight.isBeingUsed)
+                {
+                    // Make sure to only chase when the player is shining their flashlight at the Locker.
+                    Vector3 directionToLocker =
+                        transform.position - closestPlayer.transform.position;
+                    float angle = Vector3.Angle(transform.forward, directionToLocker);
+
+                    if (Mathf.Abs(angle) < closestPlayer.gameplayCamera.fieldOfView)
+                    {
+                        Target(closestPlayer.transform.position);
+                    }
+                }
+            }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (DebugToCamera)
+            {
+                DebugToCamera = false;
+
+                Target(Camera.main.transform.position);
+            }
             // Override in case I want to manually change states through the inspector.
             if (state != lastState)
             {
                 SwitchState(state);
+
+                lastState = state;
             }
 
             // Handle over-time state changes.
             switch (state)
             {
                 case LockerState.Dormant:
+                    // Fade out eye.
                     currentEyeColor = Color.Lerp(currentEyeColor, eyeColorDormant, Time.deltaTime);
                     currentEyeIntensity = Mathf.Lerp(currentEyeIntensity, 0, Time.deltaTime);
 
@@ -150,7 +216,25 @@ namespace Locker.MonoBehaviours
 
                     break;
 
-                case LockerState.Active:
+                case LockerState.Activating:
+                    // Make sure we only spin after the windup has completed.
+                    if (activationTimer > activationSpinWindup)
+                    {
+                        // Increase the rotation speed over time.
+                        currentRotationSpeed = Mathf.Lerp(
+                            currentRotationSpeed,
+                            maxRotationSpeed,
+                            Time.deltaTime / Mathf.Abs(maxRotationSpeed - currentRotationSpeed)
+                        );
+
+                        transform.rotation = Quaternion.Slerp(
+                            transform.rotation,
+                            targetRotation,
+                            Time.deltaTime * currentRotationSpeed * 4
+                        );
+                    }
+
+                    // Activate the eye color and brightness.
                     currentEyeColor = Color.Lerp(currentEyeColor, eyeColorDetect, Time.deltaTime);
                     currentEyeIntensity = Mathf.Lerp(currentEyeIntensity, 100000, Time.deltaTime);
 
@@ -169,6 +253,16 @@ namespace Locker.MonoBehaviours
                         currentEyeIntensity,
                         500000,
                         Time.deltaTime * 2
+                    );
+
+                    // Tilt the Locker up while chasing.
+                    Quaternion targetRotationChasing =
+                        targetRotation * Quaternion.Euler(Vector3.back * 8f);
+
+                    transform.rotation = Quaternion.Slerp(
+                        transform.rotation,
+                        targetRotationChasing,
+                        Time.deltaTime * 4
                     );
 
                     // Fade in our internal lights quickly.
@@ -190,8 +284,25 @@ namespace Locker.MonoBehaviours
 
                     break;
 
-                case LockerState.Closing:
+                case LockerState.Resetting:
                 case LockerState.Consuming:
+                    if (consumeTimer > consumeBloodWindup && !consumeBloodTriggered)
+                    {
+                        // Set the consuming VFX.
+                        foreach (VisualEffect vfx in visualEffects)
+                        {
+                            vfx.SendEvent(consumeVFXBeginTrigger.name);
+                        }
+
+                        consumeBloodTriggered = true;
+                    }
+                    // Reset the rotation to the target after chase.
+                    transform.rotation = Quaternion.Slerp(
+                        transform.rotation,
+                        targetRotation,
+                        Time.deltaTime * 10
+                    );
+
                     currentEyeColor = Color.Lerp(currentEyeColor, eyeColorChase, Time.deltaTime);
                     currentEyeIntensity = Mathf.Lerp(currentEyeIntensity, 0, Time.deltaTime);
 
@@ -217,15 +328,136 @@ namespace Locker.MonoBehaviours
             eyeMaterial.SetColor("_EmissiveColor", currentEyeColor * currentEyeIntensity);
         }
 
-        private void SwitchState(LockerState state)
+        private void FixedUpdate()
+        {
+            // Handle logic and required state changes.
+            switch (state)
+            {
+                case LockerState.Activating:
+                    activationTimer += Time.fixedDeltaTime;
+
+                    if (activationTimer > activationDuration)
+                    {
+                        SwitchState(LockerState.Chasing);
+                    }
+
+                    break;
+
+                case LockerState.Chasing:
+                    currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, Time.fixedDeltaTime);
+
+                    // Move towards the target location.
+                    transform.position = Vector3.MoveTowards(
+                        transform.position,
+                        targetLocation,
+                        currentSpeed
+                    );
+
+                    if (Vector3.Distance(transform.position, targetLocation) < .1f)
+                    {
+                        SwitchState(LockerState.Resetting);
+                    }
+
+                    break;
+
+                case LockerState.Consuming:
+                    consumeTimer += Time.fixedDeltaTime;
+
+                    if (consumeTimer > consumeDuration)
+                    {
+                        SwitchState(LockerState.Dormant);
+                    }
+
+                    break;
+
+                case LockerState.Resetting:
+                    resetTimer += Time.fixedDeltaTime;
+
+                    if (resetTimer > resetDuration)
+                    {
+                        SwitchState(LockerState.Dormant);
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            switch (state) // Handle collisions with a player during our dormant state.
+            {
+                case LockerState.Dormant: // We were touched by a player, target their position.
+                    if (collision.gameObject.GetComponent<PlayerControllerB>())
+                        Target(collision.transform.position);
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            switch (state) // Handle collisions with a player during our chasing state.
+            {
+                // Are we in our chase phase?
+                case LockerState.Chasing:
+                    // Check if what we collided with has a player controller.
+                    PlayerControllerB playerController =
+                        collider.gameObject.GetComponent<PlayerControllerB>();
+
+                    // If we collided with a player controler, consume it.
+                    if (playerController != null)
+                    {
+                        Consume(playerController);
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public void SwitchState(LockerState state)
         {
             // Handle one-shot state changes.
             this.state = state;
             if (state != lastState)
             {
+                // Reset timers.
+                activationTimer = 0;
+                consumeTimer = 0;
+                resetTimer = 0;
+
                 switch (state)
                 {
+                    case LockerState.Debug:
+                        // Temporarily set the target.
+                        Target(
+                            new Vector3(
+                                Random.Range(-25, 25),
+                                transform.position.y,
+                                Random.Range(-25, 25)
+                            )
+                        );
+
+                        print($"Set current debug Locker target to: {targetLocation}");
+
+                        break;
+
                     case LockerState.Dormant:
+                        // Reset our audio source.
+                        audioSource.loop = false;
+
+                        // Reset speed values.
+                        currentSpeed = 0;
+                        currentRotationSpeed = 0;
+
                         // Make sure to disable the scrape lights.
                         foreach (Light light in scrapeLights)
                         {
@@ -243,16 +475,23 @@ namespace Locker.MonoBehaviours
                             vfx.SendEvent(chaseVFXEndTrigger.name);
                         }
 
+                        consumeBloodTriggered = false;
+
                         break;
 
-                    case LockerState.Active:
+                    case LockerState.Activating:
+                        // Loop the chase audio.
+                        audioSource.PlayOneShot(AudioClipActivate);
+
                         animationController.SetTrigger("Activate");
 
                         break;
 
                     case LockerState.Chasing:
-                        // Play the chasing sound.
-                        audioSource.pitch = Random.Range(1f, 1f);
+                        // Loop the chase audio.
+                        audioSource.pitch = 1;
+                        audioSource.clip = AudioClipChase;
+                        audioSource.loop = true;
                         audioSource.Play();
 
                         // Flip open the doors for the chase to begin.
@@ -276,7 +515,16 @@ namespace Locker.MonoBehaviours
 
                         break;
 
-                    case LockerState.Closing:
+                    case LockerState.Resetting:
+                    case LockerState.Consuming:
+                        // Stop the previous looping chase audio.
+                        audioSource.Stop();
+                        audioSource.loop = false;
+
+                        // Tilt the Locker forward on stopping.
+                        transform.rotation =
+                            targetRotation * Quaternion.Euler(Vector3.forward * 20f);
+
                         animationController.SetBool("Chasing", false);
                         animationController.SetTrigger("CloseDoors");
 
@@ -286,13 +534,15 @@ namespace Locker.MonoBehaviours
                             vfx.SendEvent(chaseVFXEndTrigger.name);
                         }
 
-                        break;
-
-                    case LockerState.Consuming:
-                        // Set the consuming VFX.
-                        foreach (VisualEffect vfx in visualEffects)
+                        if (state == LockerState.Consuming) // Activate the consume specific effects.
                         {
-                            vfx.SendEvent(consumeVFXBeginTrigger.name);
+                            // Play the consuming sound effect.
+                            audioSource.PlayOneShot(AudioClipConsume);
+                        }
+                        else
+                        {
+                            // Play reset audio clip.
+                            audioSource.PlayOneShot(AudioClipReset);
                         }
 
                         break;
@@ -305,18 +555,53 @@ namespace Locker.MonoBehaviours
             }
         }
 
-        public void PlayerScan(GameObject player, Camera camera) { }
-
-        public void Activate(GameObject player)
+        public void Target(Vector3 position)
         {
-            // Activate the locker and begin the attack sequence.
-            targetPlayer = player;
+            if (position.y - transform.position.y > 2 || position.y - transform.position.y < 0) // Don't target entities higher or lower.
+                return;
+
+            position.y = transform.position.y; // Make sure we don't change in elevation.
+
+            // Only allowing targeting during the debug or dormant state.
+            if (state == LockerState.Dormant || state == LockerState.Debug)
+            {
+                // Activate the locker and begin the attack sequence.
+                targetLocation = position;
+
+                // Set the target rotation.
+                targetRotation = Quaternion.LookRotation(targetLocation - transform.position);
+
+                // Rotate an additional 90 degree offset.
+                targetRotation *= Quaternion.Euler(Vector3.up * 90);
+
+                SwitchState(LockerState.Activating);
+            }
         }
 
-        public void Chase(GameObject player)
+        public void Consume(PlayerControllerB player)
         {
-            // Chase to a player's last position.
-            targetPlayer = player;
+            // Only allow consuming during the chase state.
+            if (state == LockerState.Chasing)
+            {
+                // Kill the player.
+                player.KillPlayer(Vector3.zero, false, CauseOfDeath.Crushing, 0);
+
+                SwitchState(LockerState.Consuming);
+            }
+        }
+
+        public void PlayerScan(GameObject player)
+        {
+            // Only allowing activation during the dormant state.
+            if (state == LockerState.Dormant || state == LockerState.Debug)
+            {
+                if (HasLineOfSightToPosition(player.transform.position))
+                {
+                    currentEyeColor = eyeColorScan;
+
+                    Target(player.transform.position);
+                }
+            }
         }
     }
 }
